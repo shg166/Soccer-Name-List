1e5b01ec294c877e264252e0a7b5a34177f80c01
adon <86583577+shg166@users.noreply.github.com>
2023-06-19 18:47:09 +0900

    Add Code & Name List

---
+++ Mod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using ColossalFramework.UI;
+using ICities;
+
+namespace JP_SoccerNameList
+{
+	public class JP_SoccerNameList : IUserMod
+	{
+
+		public string Name
+		{
+			get { return "JP Soccer Name List (CNL) v1.0"; }
+		}
+
+		public string Description
+		{
+			get { return "Custom Name Listsを使って、DLC Mtach Dayの対戦相手のチーム名を変更します"; }
+		}
+
+	}
+}
+
